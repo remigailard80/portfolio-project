fcc74a8fea9831e8a57d1eca2acc2b3d0f75a563
김민석 <kimminseok@gimminseog-ui-MacBookAir.local>
2021-07-26 16:34:42 +0900

    first commit

--- containers/Home/Banner1.tsx
+++ containers/Home/Banner1.tsx
@@ -6,6 +6,8 @@ export default function Banner1() {
         <>
             <SliderBanner class="SliderBanner" backgroundColor={'rgba(152,0,0,0.56)'} type={0}/>
             <SliderBanner class="SliderBanner" backgroundColor={'white'} type={1}/>
+            <SliderBanner class="SliderBanner" backgroundColor={'rgba(152,0,0,0.56)'} type={0}/>
+            <SliderBanner class="SliderBanner" backgroundColor={'white'} type={1}/>
         </>
     )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,62 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  // state
+  let [currentSlide, setcurrentSlide] = useState(1);
+  let [loading, setLoading] = useState(false);
+
+  // Slider 이동 증가량 조절
+  function slideFunction (x, target) {
+    let result = x/(Math.log2(x));
+    if (result < target/3) {
+      return result*1.3
+    }
+    return result;
+  }
+
   useEffect(() => {
     let Slides = document.querySelectorAll('.SliderBanner');
-    window.addEventListener('click', (e) => {
-      console.log(Slides);
-    })
-    console.log("App upDated");
+    let i = 2;
+    let threshold = 150;
+
+    const SliderPaging = () => {
+      let scrollY = window.pageYOffset;
+      let nextSlideTop = Slides[currentSlide].offsetTop;
+      
+      if (loading == false && scrollY < nextSlideTop) {
+        setTimeout(() => {
+          if (loading == false) {
+            i += slideFunction(i, nextSlideTop);
+            window.scrollTo(0,Slides[currentSlide-1].offsetTop+i);
+            console.log(i, scrollY, nextSlideTop);
+          }
+          if (scrollY>= nextSlideTop) {
+            console.log('----------');
+            console.log(i, scrollY, nextSlideTop);
+            setLoading(true);
+          }
+        }, 2);
+      } else {
+        console.log('----------');
+        window.scrollTo(0, nextSlideTop);
+        let resetLoad;
+        if (resetLoad) {
+          console.log('rrrrr');
+          clearTimeout(resetLoad);
+        }
+        resetLoad = setTimeout(() => {
+          setLoading(false);
+          setcurrentSlide(currentSlide+1);
+          i = 2;
+        }, 1500);
+        resetLoad;
+      };
+    };
+
+    window.addEventListener('scroll', SliderPaging);
   })
   return <Component {...pageProps} />
 }
